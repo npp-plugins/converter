7c6bd01aaa64030f0c8c88e29815048e658ede66
Alexander Gavrilov <wowemuh@gmail.com>
2022-11-26 20:10:34 +0300

    Fix typo
    Close #13

--- source/PluginDefinition.cpp
+++ source/PluginDefinition.cpp
@@ -149,11 +149,11 @@ void loadConfFile()
 	confPath += pluginConfName;
 	
 	const char confContent[] = "\
-; This section contains the paremters for command ASCII -> Hex\n\
+; This section contains the parameters for command ASCII -> Hex\n\
 ; If you modify directly this file, please restart your Notepad++ to take effect.\n\
 ; * insertSpace: this parameter allows you to insert a white space between the generated hex codes. Set the value to 1 to enable it, 0 otherwise.\n\
 ; * uppercase: this parameter allows you to make a-f in UPPERCASE (ie. A-F). Set the value to 1 to enable it , 0 otherwise.\n\
-; * nbCharPerLine:this parameter allows you to break line. The value you set is the number of ascii character per line. Set the value from 0 to whatever you want.\n\
+; * nbCharPerLine: this parameter allows you to break line. The value you set is the number of ascii character per line. Set the value from 0 to whatever you want.\n\
 [ascii2Hex]\n\
 insertSpace=0\n\
 uppercase=1\n\
